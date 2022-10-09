55170a56b5189d2ab1a824d95aa2c86cfb5814b4
Thomas Mailund <mailund@birc.au.dk>
2022-10-09 13:45:53 +0200

    restored

--- src/test_genome.py
+++ src/test_genome.py
@@ -59,8 +59,3 @@ def test_list_genome() -> None:
 def test_linked_list_genome() -> None:
     """Test that the linked list implementation works."""
     run_genome_test(LinkedListGenome)
-
-
-def test_test() -> None:
-    """Fail on purpose."""
-    assert False
